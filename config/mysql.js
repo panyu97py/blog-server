cb78e2d9399a5c1f5e131980f2eeeb77a899a123
yu.pan <yu.pan@hzjo.com>
2018-12-26 20:53:20 +0800

    add module mysql

---
+++ config/mysql.js
@@ -0,0 +1,36 @@
+
+const mysql = require('mysql')
+const pool = mysql.createPool({
+  host     :  '127.0.0.1',
+  user     :  'root',
+  password :  '123456',
+  database :  'database'
+})
+
+let query = function( sql, values ) {
+  return new Promise(( resolve, reject ) => {
+    pool.getConnection(function(err, connection) {
+      if (err) {
+        reject( err )
+      } else {
+        connection.query(sql, values, ( err, rows) => {
+
+          if ( err ) {
+            reject( err )
+          } else {
+            resolve( rows )
+          }
+          connection.release()
+        })
+      }
+    })
+  })
+}
+
+module.exports = query
+// async function getData( ) {
+//   let sql = 'SELECT * FROM table'
+//   let data = await query( sql )
+//   console.log(data)
+// }
+// getData()
