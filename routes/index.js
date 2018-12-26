e9ee7188f2db437dae73ee20e90ee1417083cd60
yu.pan <yu.pan@hzjo.com>
2018-12-26 16:03:54 +0800

    modify router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@
  * 集合所有子路由
  */
 const router = require('koa-router')()
-const users =require('./users')
+
 //添加用户模块至路由表
+const users =require('./users')
 router.use('/users', users.routes(), users.allowedMethods());
 
 module.exports = router
