fcce06e631c2c04da349f4300cf8c7b6cadf96a4
yu.pan <yu.pan@hzjo.com>
2019-01-21 20:59:52 +0800

    modify mysql password

--- config/mysql.js
+++ config/mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const pool = mysql.createPool({
   host     :  '127.0.0.1',
   user     :  'root',
-  password :  'panyu',
+  password :  'F6F43c937e1a',
   database :  'blog'
 })
 
